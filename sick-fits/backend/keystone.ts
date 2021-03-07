392e2b6477b01af795af1c136c83e6bee6b5628e
Andrei Fidelman <afidelman@microsoft.com>
2021-03-07 23:41:16 +0100

    Setting up Keystone and Typescript

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { createSchema, config } from "@keystone-next/keystone/schema";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
