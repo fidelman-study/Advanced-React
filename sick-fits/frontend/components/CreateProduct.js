8b3aac5a4419e098b1d4cce890b7c19859719149
Andrei Fidelman <afidelman@microsoft.com>
2021-03-14 18:11:02 +0100

    Hooking up our File input and Form Styles

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,11 +1,68 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
-export function CreateProduct() {
-  const { inputs, handleChange, clearForm, resetForm } = useForm({
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm } = useForm({
     image: '',
     name: 'Nice Shoes',
     price: 34234,
     description: 'These are the best shoes!',
   });
-  return <form />;
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit the inputfields to the backend:
+        clearForm();
+        // Go to that product's page!
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
 }
