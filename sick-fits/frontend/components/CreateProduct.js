0aead0f4d98df9e07d0b732f9d572f02bdf42893
Andrei Fidelman <afidelman@microsoft.com>
2021-03-14 17:41:21 +0100

    A real good lesson in React Forms and Custom Hooks

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,11 @@
+import useForm from '../lib/useForm';
+
+export function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Nice Shoes',
+    price: 34234,
+    description: 'These are the best shoes!',
+  });
+  return <form />;
+}
