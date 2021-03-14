10805338c5b048b1322430c0041a6c48a8f65b3b
Andrei Fidelman <afidelman@microsoft.com>
2021-03-14 14:36:50 +0100

    Fixing and Styling the Nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 
+import NavStyles from './styles/NavStyles';
+
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
-    </nav>
+      <>
+        <Link href="/sell">Sell</Link>
+        <Link href="/orders">Orders</Link>
+        <Link href="/account">Account</Link>
+      </>
+    </NavStyles>
   );
 }
