c6db5dfb53a73ba0cd349f8ea77f18a1f863eb11
Andrei Fidelman <afidelman@microsoft.com>
2021-03-29 19:27:25 +0200

    Cart - Displaying Items in a Custom Component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,11 +1,13 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import SignOut from './SignOut';
-import { useUser } from './User';
-import NavStyles from './styles/NavStyles';
+import SignOut from "./SignOut";
+import { useUser } from "./User";
+import NavStyles from "./styles/NavStyles";
+import { useCart } from "../lib/cartState";
 
 export default function Nav() {
   const user = useUser();
+  const { openCart } = useCart();
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
@@ -16,6 +18,17 @@ export default function Nav() {
           <Link href="/account">Account</Link>
 
           <SignOut />
+
+          <button type="button" onClick={openCart}>
+            My Cart
+            {/* <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) =>
+                  tally + (cartItem.product ? cartItem.quantity : 0),
+                0
+              )}
+            /> */}
+          </button>
         </>
       )}
 
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,20 +1,23 @@
-import NProgress from 'nprogress';
-import Router from 'next/router';
-import { ApolloProvider } from '@apollo/client';
-import Page from '../components/Page';
-import '../components/styles/nprogress.css';
-import withData from '../lib/withData';
+import NProgress from "nprogress";
+import Router from "next/router";
+import { ApolloProvider } from "@apollo/client";
+import Page from "../components/Page";
+import "../components/styles/nprogress.css";
+import withData from "../lib/withData";
+import { CartStateProvider } from "../lib/cartState";
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </ApolloProvider>
   );
 }
