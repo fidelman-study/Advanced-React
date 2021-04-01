1b923d0ca761fddd0a3c97c407a388f016e1aa44
Andrei Fidelman <afidelman@microsoft.com>
2021-04-01 05:09:35 +0200

    Cart - Adding Items To Cart in React

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -1,10 +1,12 @@
-import Link from 'next/link';
-import ItemStyles from './styles/ItemStyles';
-import Title from './styles/Title';
-import PriceTag from './styles/PriceTag';
+import Link from "next/link";
+import ItemStyles from "./styles/ItemStyles";
+import Title from "./styles/Title";
+import PriceTag from "./styles/PriceTag";
 
-import formatMoney from '../lib/formatMoney';
-import DeleteProduct from './DeleteProduct';
+import formatMoney from "../lib/formatMoney";
+import DeleteProduct from "./DeleteProduct";
+
+import AddToCart from "./AddToCart";
 
 export default function Product({ product }) {
   return (
@@ -21,7 +23,7 @@ export default function Product({ product }) {
       <div className="buttonList">
         <Link
           href={{
-            pathname: '/update',
+            pathname: "/update",
             query: {
               id: product.id,
             },
@@ -29,6 +31,7 @@ export default function Product({ product }) {
         >
           Edit ✏️
         </Link>
+        <AddToCart id={product.id} />
         <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
